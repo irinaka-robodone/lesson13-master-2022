73a3c63d75755eae6d682a5fc796828d2be5fff0
Irinaka Robdone <irinaka.robodone@gmail.com>
2022-09-22 20:06:34 +0900

    init commit by ki

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def get_filepath():
     print(filepath)
     message["text"]=filepath
 
+def callback_func(event):
+    label["text"]=event.keysym
+
 def msg_info():
     ret=label["text"]=str(messagebox.showinfo("info","インフォメーション"))
     print(ret)
@@ -50,14 +53,24 @@ def msg_retrycannel():
         label["text"]="cancel"
 
 root = tk.Tk()
-root.title("実験")
+root.title("キーボード入力")
 root.geometry("800x400")
+
+label=tk.Label(root,text="keysym",relief="groove")
+
+canvas = tk.Canvas(root,width=500,height=500,bg="lime green")
+canvas_2 = tk.Canvas(root,width=200,height=30,bg="orange")
+canvas.pack()
+canvas_2.place(x=300,y=200)
+
+frame1=tk.Frame(root,width=700,height=400,background="red")
+frame2=tk.Frame(root,width=700,height=400,background="yellow")
 
 message=tk.Message(root,text="file_path",width=600)
 
 bt=tk.Button(text="ファイルダイアログ",command=get_filepath)
 
-label=tk.Label(text="レイアウト設定",background="yellow")
+#label=tk.Label(text="レイアウト設定",background="yellow")
 bt1=tk.Button(text="info",command=msg_info)
 bt2=tk.Button(text="warning",command=msg_warning)
 bt3=tk.Button(text="error",command=msg_error)
@@ -66,40 +79,72 @@ bt5=tk.Button(text="yesno",command=msg_yesno)
 bt6=tk.Button(text="okcancel",command=msg_okcancel)
 bt7=tk.Button(text="retrycancel",command=msg_retrycannel)
 
-label00=tk.Label(text="row=0,column=0",relief="groove")
-label01=tk.Label(text="row=0,column=1",relief="groove")
-label02=tk.Label(text="row=0,column=2",relief="groove")
+label00=tk.Label(frame1,text="row=0,column=0",relief="groove")
+label01=tk.Label(frame1,text="row=0,column=1",relief="groove")
+label02=tk.Label(frame1,text="row=0,column=2",relief="groove")
 
-label10=tk.Label(text="row=1,column=0",relief="groove")
-label11=tk.Label(text="row=1,column=1",relief="groove")
-label12=tk.Label(text="row=1,column=2",relief="groove")
+label10=tk.Label(frame1,text="row=1,column=0",relief="groove")
+label11=tk.Label(frame1,text="row=1,column=1",relief="groove")
+label12=tk.Label(frame1,text="row=1,column=2",relief="groove")
 
-label20=tk.Label(text="row=2,column=0",relief="groove")
-label21=tk.Label(text="row=2,column=1",relief="groove")
-label22=tk.Label(text="row=2,column=2",relief="groove")
+label00_2=tk.Label(frame2,text="row=0,column=0",relief="groove")
+label01_2=tk.Label(frame2,text="row=0,column=1",relief="groove")
+label02_2=tk.Label(frame2,text="row=0,column=2",relief="groove")
+
+label10_2=tk.Label(frame2,text="row=1,column=0",relief="groove")
+label11_2=tk.Label(frame2,text="row=1,column=1",relief="groove")
+label12_2=tk.Label(frame2,text="row=1,column=2",relief="groove")
+
+# label=tk.Label(frame1,text="四角1",background="white")
+# label.place(x=25,y=25,width=50,height=50)
+
+# label=tk.Label(frame1,text="四角2",background="white")
+# label.place(x=350,y=100,width=230,height=200)
+
+# label=tk.Label(frame2,text="四角3",background="white")
+# label.place(x=25,y=25,width=50,height=50)
+
+# label=tk.Label(frame2,text="四角4",background="white")
+# label.place(x=350,y=100,width=230,height=200)
+
+# canvas.create_rectangle(20,40,150,300,fill="red",outline="blue",width=3)
+# canvas.create_oval(100,100,350,250,fill="blue",outline="white",width=10)
+# canvas.create_line(450,50,40,300,150,370,100,450,fill="blue",width=3)
+
+# canvas.create_text(100,400,text="プログラミング",font=("Helvetica",18,"bold"),angle=-30)
+# canvas.create_text(400,60,text="ロボット制作",font=("Helvetica",18,"bold"),angle=90)
+# canvas.create_text(400,400,text="ロボ団",font=("Heletica",18,"bold"),angle=180)
+
+root.bind("<Key>",callback_func)
 
 # message.pack()
-# label.pack(padx=10,pady=100,ipadx=100,ipady=10,fill="x",expand=True)
+label.pack()
 # bt.pack()
 
-label00.grid(row=0,column=0)
-label01.grid(row=0,column=1)
-label02.grid(row=0,column=2)
+# label00.grid(row=0,column=0,columnspan=3)
+# label10.grid(row=0,column=1)
+# label11.grid(row=0,column=2)
+# label12.grid(row=1,column=0)
 
-label10.grid(row=1,column=0)
-label11.grid(row=1,column=1)
-label12.grid(row=1,column=2)
+# label00_2.grid(row=0,column=0,rowspan=2)
+# label01_2.grid(row=0,column=1)
 
-label20.grid(row=2,column=0)
-label21.grid(row=2,column=1)
-label22.grid(row=2,column=2)
+# label10_2.grid(row=0,column=2)
+# label11_2.grid(row=1,column=1)
+# label12_2.grid(row=1,column=2)
 
-#bt1.pack()
-#bt2.pack()
-#bt3.pack()
-#bt4.pack()
-#bt5.pack()
-#bt6.pack()
-#bt7.pack()
+# frame1.pack()
+# frame2.pack()
+
+# bt1.pack()
+# bt2.pack()
+# bt3.pack()
+# bt4.pack()
+# bt5.pack()
+# bt6.pack()
+# bt7.pack()
+
+
+
 
 root.mainloop()
